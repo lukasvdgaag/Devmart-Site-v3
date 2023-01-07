111bed6068366c6473c81e7df9110ebaf96e0a17
GCNT <lukasvdgaag@gmail.com>
2023-01-08 00:55:30 +0100

    Added BBCode support.

--- resources/js/components/Common/BBCode.vue
+++ resources/js/components/Common/BBCode.vue
@@ -1,76 +1,23 @@
 <template>
-    <div class="bbcode markdown">
-        {{bbcode}}
+    <div class="bbcode markdown" v-html="BBCodeParser.parse(source, true)">
     </div>
 </template>
 
 <script>
-import core from '@bbob/core'
-import * as html from '@bbob/html'
-import { isStringNode, isTagNode } from '@bbob/plugin-helper';
+import BBCodeParser from "@/services/BBCodeParser";
 
 export default {
     name: "BBCode",
-
     computed: {
-        bbcode() {
-            if (this.$slots.default) {
-                const source = this.$slots.default.reduce((acc, vnode) => acc + vnode.text, '');
-                return this.render(createElement('span'), source, this.plugins, this.options);
-            }
-        }
-    },
-
-    methods: {
-        toAST(source, plugins = [], options = {}) {
-            return core(plugins).process(source, {
-                ...options,
-                render: (input) => html.render(input, {stripTags: true});
-            }).tree;
-        },
-        isContentEmpty(content) {
-            return !content || content.length === 0;
-        },
-        tagToVueNode(createElement, node, index) {
-            const { class: className, style, ...domProps } = node.attrs || {};
-
-            return createElement(
-                node.tag,
-                {
-                    key: index,
-                    class: className,
-                    style,
-                    domProps,
-                },
-                this.isContentEmpty(node.content) ? null : this.renderToVueNodes(createElement, node.content),
-            );
-        },
-        renderToVueNodes(createElement, nodes) {
-            return [].concat(nodes).reduce((arr, node, index) => {
-                if (isTagNode(node)) {
-                    arr.push(this.tagToVueNode(createElement, node, index));
-                } else if (isStringNode(node)) {
-                    arr.push(node);
-                }
-
-                return arr;
-            }, []);
-        },
-        render(createElement, source, plugins, options) {
-            return this.renderToVueNodes(createElement, this.toAST(source, plugins, options));
+        BBCodeParser() {
+            return BBCodeParser
         }
     },
 
     props: {
-        container: {
+        source: {
             type: String,
-            default: 'span'
-        },
-        plugins: {
-            type: Array,
-        },
-        options: {
-            type: Object,
+            required: true,
         }
     }
 }
--- resources/js/components/Pages/Plugins/Views/PluginOverviewPage.vue
+++ resources/js/components/Pages/Plugins/Views/PluginOverviewPage.vue
@@ -1,10 +1,25 @@
 <template>
-
+    <BBCode :source="plugin.features">
+    </BBCode>
 </template>
 
 <script>
+import BBCode from "@/components/Common/BBCode.vue";
+
 export default {
-    name: "PluginOverviewPage"
+    name: "PluginOverviewPage",
+    components: {BBCode},
+
+    props: {
+        plugin: {
+            type: Object,
+            required: true,
+        },
+        pluginId: {
+            type: Number,
+            required: true,
+        }
+    }
 }
 </script>
 
