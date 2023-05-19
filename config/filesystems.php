7b36a3e3858995729faa9cf073105da11cc977a4
GCNT <lukasvdgaag@gmail.com>
2023-05-19 15:11:17 +0200

    Added asset storage folder.

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,8 +32,11 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+//            'root' => storage_path('app'),
+            'root' => public_path('assets/storage'),
+            'url' => env('APP_URL').'/assets/storage',
             'throw' => false,
+            'visibility' => 'public',
         ],
 
         'public' => [
